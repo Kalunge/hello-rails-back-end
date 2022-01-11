a33777f02bc5000648cb493c5b4179dab7a1bd6a
kalunge <muthomititus+github@gmail.com>
2022-01-11 15:12:24 +0100

    Update Stylelint

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,5 +4,5 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-greetings = Greeting.create!([{message: "Hello and welcome to the rails backend"}, {message: "Did you know that You can connect React and Rails?"}, {message: "As you might have guessed Rails and React are best of Friends"}, {message: "Thanks for visiting the Site and have a good time connecting rails to React"}, {message: "THis might be the last greeting anyway keep trying"}])
+greetings = Greeting.create!([{message: "Hello and welcome to the rails backend"}, {message: "Did you know that You can connect React and Rails ?"}, {message: "As you might have guessed Rails and React are best of Friends"}, {message: "Thanks for visiting the Site and have a good time connecting rails to React"}, {message: "THis might be the last greeting anyway keep trying"}])
 #   Character.create(name: 'Luke', movie: movies.first)
